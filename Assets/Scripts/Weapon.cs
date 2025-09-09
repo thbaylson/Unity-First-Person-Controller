b20b3d77fe220aa8358a8cb36e92e5b13289f945
Tyler Baylson <thbaylson@gmail.com>
2025-09-08 23:49:00 -0400

    Lesson 6.7: Shoot Input. Modified the starter assets to include player input for shooting.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,23 +1,26 @@
+using StarterAssets;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    StarterAssetsInputs starterAssetsInputs;
+
+    void Awake()
     {
-        
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        if (Input.GetMouseButtonDown(0))
+        if (starterAssetsInputs.shoot)
         {
+            RaycastHit hit;
             if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
             {
                 Debug.Log("We hit " + hit.transform.name);
             }
+            starterAssetsInputs.ShootInput(false);
         }
     }
 }
