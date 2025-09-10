8ed5e921c8e336e16999e003951373a78ecabdc2
Tyler Baylson <thbaylson@gmail.com>
2025-09-09 22:58:17 -0400

    Lesson 6.10: Weapon Animation. Added a kickback animation to the player's gun.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damageAmount = 1;
 
     private StarterAssetsInputs starterAssetsInputs;
+
+    const string ANIM_SHOOT = "Shoot";
 
     void Awake()
     {
@@ -24,12 +27,14 @@ public class Weapon : MonoBehaviour
         if (!starterAssetsInputs.shoot) { return; }
 
         muzzleFlash.Play();
+        animator.Play(ANIM_SHOOT, 0, 0f);
 
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
             hit.transform.GetComponent<EnemyHealth>()?.TakeDamage(damageAmount);
         }
+
         starterAssetsInputs.ShootInput(false);
     }
 }
